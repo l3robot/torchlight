2a887f5d070b4585436c5f64b3c3e813b1bcf2d7
l3robot <emile.courriel@gmail.com>
2018-04-08 19:13:13 -0400

    add a caching DatasetFolder + few corrections

--- torchlight/io/folder.py
+++ torchlight/io/folder.py
@@ -4,13 +4,28 @@ import torchvision
 class CachingImageFolder(torchvision.datasets.ImageFolder):
 
     def __init__(self, *args, **kwargs):
-        super(CachingImageFolder, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.cached = {}
 
     def __getitem__(self, index):
         if index in self.cached:
             return self.cached[index] 
         else:
-            img = super(CachingImageFolder, self).__getitem__(index)
+            img = super().__getitem__(index)
             self.cached[index] = img
             return img
+
+
+class CachingDatasetFolder(torchvision.datasets.DatasetFolder):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.cached = {}
+
+    def __getitem__(self, index):
+        if index in self.cached:
+            return self.cached[index] 
+        else:
+            obj = super().__getitem__(index)
+            self.cached[index] = obj
+            return obj
